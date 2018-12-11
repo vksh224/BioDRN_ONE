e124565e7c26d4fe48a779d2c505e29c91136b5f
Vijay <vjsah27@gmail.com>
2018-12-10 23:11:15 -0500

    minor changes to setting and del ratio fiels

--- BioDRN/src/del_ratio.py
+++ BioDRN/src/del_ratio.py
@@ -57,8 +57,8 @@ def get_energy_stat(file_name, time):
 		reader = csv.reader(report, delimiter = ' ')
 		for line in reader:
 			if abs(float(line[0]) - float(time)) < 1 and line[1].find('available_energy') == 0:
-				available_energy = line[2]
-				alive_nodes = line[3]
+				available_energy = float(line[2])
+				alive_nodes = float(line[3])
 				break
 	
 	# print("time", time, "energy", available_energy, "nodes", alive_nodes)
@@ -70,57 +70,60 @@ def get_energy_stat(file_name, time):
 folders = ('Nepal_Orig', 'Nepal_Bio')
 routers = ('BioDRNRouter',)
 endTimes = ('900', '1800', '2700', '3600',)
-
-# %%Group.router%%_%%Scenario.endTime%%
+generators = ('MessageBurstGenerator',)
+rngSeed =('1', )
+# Scenario.name = %%Group.router%%_%%Scenario.endTime%%_%%Events1.class%%_%%MovementModel.rngSeed%%
 
 save_folder = "Users/vijay/BioDRN_Code"
 for folder in folders:
 	print("\nFolder " + folder)
 	for router in routers:
-		
-		del_ratio = []
-		latency = []
-		hop_count = []
-		overhead = []
-		available_energy_list = []
-		alive_nodes_list = []	
-		for time in endTimes:
-		    fname = "reports/%s/%s_%s_MessageStatsReport.txt" % (folder, router, time)
-		    if os.path.isfile(fname):
-		    	del_ratio.append(get_stat(fname, 'delivery_prob'))
-		    	latency.append(get_stat(fname, "latency_avg"))
-		    	hop_count.append(get_stat(fname, "hopcount_avg"))
-		    	overhead.append(get_stat(fname, "overhead_ratio"))
+		for generator in generators:
+			del_ratio = []
+			latency = []
+			hop_count = []
+			overhead = []
+			available_energy_list = []
+			alive_nodes_list = []	
+			for time in endTimes:
+				for rng in rngSeed:
+				    fname = "reports/%s/%s_%s_%s_%s_MessageStatsReport.txt" % (folder, router, time, generator, rng)
+				    if os.path.isfile(fname):
+				    	del_ratio.append(get_stat(fname, 'delivery_prob'))
+				    	latency.append(get_stat(fname, "latency_avg"))
+				    	hop_count.append(get_stat(fname, "hopcount_avg"))
+				    	overhead.append(get_stat(fname, "overhead_ratio"))
 
-		    else:
-		    	print("Stat file not found", fname)
+				    else:
+				    	print("Stat file not found", fname)
 
-		    energyfname = "reports/%s/%s_%s_EnergyLevelReport.txt" % (folder, router, time)
-		    if os.path.isfile(fname):
-		    	available_energy, alive_nodes = get_energy_stat(energyfname, time)
-		    	available_energy_list.append(available_energy)
-		    	alive_nodes_list.append(alive_nodes)
+				    energyfname = "reports/%s/%s_%s_%s_%s_EnergyLevelReport.txt" % (folder, router, time, generator, rng)
+				    if os.path.isfile(fname):
+				    	available_energy, alive_nodes = get_energy_stat(energyfname, time)
+				    	available_energy_list.append(available_energy)
+				    	alive_nodes_list.append(alive_nodes)
 
-		    else:
-		    	print("Energy file not found", energyfname)
+				    else:
+				    	print("Energy file not found", energyfname)
 
-		print("PDR", del_ratio)
-		print("Lat", latency)
-		print("Over", overhead)
-		print("Ener", available_energy_list)
-		print("nodes", alive_nodes_list)
+				# print("PDR", del_ratio)
+				# print("Lat", latency)
+				# print("Over", overhead)
+				# print("Ener", available_energy_list)
+				# print("nodes", alive_nodes_list)
 
+				# Average delivery ratio
+				avg_del = get_average(del_ratio)
+				avg_latency = get_average(latency)
+				avg_hop = get_average(hop_count)
+				avg_overhead = get_average(overhead)
+				avg_available_energy = get_average(available_energy_list)
+				avg_alive_nodes = get_average(alive_nodes_list)
 
-		# Average delivery ratio
-		# avg_del = get_average(del_ratio)
-		# avg_latency = get_average(latency)
-		# avg_hop = get_average(hop_count)
-		#sd = get_std_dev(del_ratio)
-		#ci = gs.confidence_interval_mean(rng_max, sd)
+				#sd = get_std_dev(del_ratio)
+				#ci = gs.confidence_interval_mean(rng_max, sd)
 
-		
+				print ('%s %s %.2f %.2f %.2f %.2f %.2f %.2f' % (generator, time, avg_del, avg_latency, avg_hop, avg_overhead, avg_available_energy, avg_alive_nodes))
 
-			# Any two data sets for Gnuplot must be separated by exactly 2 blank lines
-			#print ('\n')
 
 
